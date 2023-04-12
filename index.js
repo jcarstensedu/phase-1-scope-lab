0006194137ddc1ee9abb13ba0c9de8fd42117d93
johnathan carstens <jcarstensedu@gmail.com>
2023-04-11 21:58:59 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'// Write your solution in this file!
+const leastFavoriteCustomer = 'tim'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(newFavorite) {
+    bestCustomer = newFavorite
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'sad'
+}
